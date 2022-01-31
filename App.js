e834335103fc1ac86ae966888d102d3075b7123e
Dusan <45889538+iceman021@users.noreply.github.com>
2022-01-31 06:54:19 +0100

    React files without node modules uploaded

---
+++ App.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import Pusher from "pusher-js";
+
+function App() {
+  const [username, setUsername] = useState('username');
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  let allMessages = [];
+
+  useEffect( () => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('f27c4e6a127e8ee07398', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('GoChatReact');
+    channel.bind('message', function(data) {
+      allMessages.push(data);
+      setMessages(allMessages);
+    });
+  }, );
+
+  const submit = async e => {
+    e.preventDefault();
+
+    await fetch('http://localhost:8000/api/messages', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username,
+        message
+      })
+    });
+
+    setMessage('');
+  }
+
+
+  return (
+ <div class="Container">
+    <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+      <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+       
+      <input className="fs-5 fw-semibold"  value={username} 
+      onChange={e => setUsername(e.target.value)}/>
+      </div>
+      <div className="list-group list-group-flush border-bottom scrollarea">
+      {messages.map(message => { 
+         return ( 
+         <div class="list-group-item list-group-item-action py-3 lh-tight">
+        
+           <div className="d-flex w-100 align-items-center justify-content-between">
+            <strong className="mb-1">{messages.username}</strong>
+           <small class="text-muted">WEDNESDAY</small>
+        </div>
+      <div className="col-10 mb-1 small">{message.message}</div>
+    </div>
+       )
+      })}
+    </div>
+  </div>
+      <form onSubmit={e => submit(e)}>
+      <input className="form-control" placeholder="Write a message" value={message}
+        onChange={e => setMessage(e.target.value)}
+        />
+      </form>
+
+
+</div>
+);
+}
+
+export default App;
